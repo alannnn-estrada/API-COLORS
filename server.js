6386a8e073eb483f9c18d82f28e40b84fea46906
alann-estrada <142933070+alann-estrada@users.noreply.github.com>
2023-11-03 01:54:10 -0600

    Modificaciones en uppercase y lowercase

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3000;
 
 colorsData = {
   Mercury: "#ff2b2b",
